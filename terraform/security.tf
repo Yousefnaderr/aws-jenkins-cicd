43d0c4e90ea4cb43a28351cb337fff9261bc098a
Youssef Nader <Youssefnaderr2002@gmail.com>
2025-11-03 14:26:10 +0200

    Remove .terraform directory and ignore it

---
+++ terraform/security.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "cicd_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Allow controlled public access and full internal access"
+  vpc_id      = aws_vpc.main.id
+
+  # SSH only from your IP
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    description = "Jenkins UI from my IP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    description = "App HTTP (public demo)"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Internal all traffic within VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+
